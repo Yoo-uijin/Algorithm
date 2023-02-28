9aedc8539c0a34e50733dc779b1f09c343f24dfd
Yoo-uijin <timetam24@naver.com>
2023-02-28 20:43:06 +0900

    Case #number: A + B = result (fast output)

---
+++ step3_loop/h11022/app.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(file).toString().trim().split("\n");
+
+// let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+
+let count = input[0];
+let numbers = [];
+
+for (let i = 1; i < input.length; i++) {
+  if (input[i] !== "") {
+    numbers.push(input[i].split(" "));
+  }
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  let num1 = Number(numbers[i][0]);
+  let num2 = Number(numbers[i][1]);
+
+  console.log(`Case #${i + 1}: ${num1} + ${num2} = ${num1 + num2}`);
+}
