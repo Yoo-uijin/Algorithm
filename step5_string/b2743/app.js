874d639469e3a4f4dfe4fc5ff958c97d905f0900
Yoo-uijin <timetam24@naver.com>
2023-03-04 01:55:50 +0900

    Find the number of characters in the input text

---
+++ step5_string/b2743/app.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(file).toString().trim();
+
+// const input = require("fs")
+//   .readFileSync("/dev/stdin")
+//   .toString()
+//   .trim();
+
+console.log(input.length);
