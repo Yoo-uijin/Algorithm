2ba79e07f03306b565ed6564f192d8a3ac31f208
Yoo-uijin <timetam24@naver.com>
2023-03-18 09:12:09 +0900

    Update app.js

--- basic_1/data-structure/okunsoo_17298/app.js
+++ basic_1/data-structure/okunsoo_17298/app.js
@@ -1,25 +1,23 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(file).toString().trim().split(`\n`);
-// const input = require("fs")
-//   .readFileSync("dev/stdin")
-//   .toString()
-//   .trim()
-//   .split(`\n`);
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const arr = input[1].split(" ");
+let cases = Number(input[0]);
+let splited = input[1].split(" ");
 let stack = [];
-let result = [];
 
-for (let i = 0; i < arr.length - 1; i++) {
-  for (let k = i + 1; k < arr.length; k++) {
-    if (arr[i] < arr[k]) {
-      stack[stack.length] = arr[k];
-    }
+for (let i = 0; i < cases; i++) {
+  while (
+    stack.length !== 0 &&
+    Number(splited[i]) > Number(splited[stack[stack.length - 1]])
+  ) {
+    splited[stack.pop()] = splited[i];
   }
-  result[result.length] = stack[0] ? stack[0] : -1;
-  stack.length = 0;
+  stack[stack.length] = i;
 }
-result[result.length] = -1;
 
-console.log(result.join(" "));
+while (stack.length !== 0) {
+  splited[stack.pop()] = -1;
+}
+console.log(splited.join(" ").trim());
